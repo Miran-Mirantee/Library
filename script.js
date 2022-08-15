361661522d254e8cfe10884c26700697952156b6
Miran-Mirantee <killerm11805@hotmail.com>
2022-08-16 00:23:40 +0700

    Created book constructor and example book

---
+++ script.js
@@ -0,0 +1,19 @@
+let library = [
+    {
+        author: 'Mirantee',
+        title: 'Javascript is FUN',
+        pages: 50,
+        read: true
+    }
+];
+
+function book(author, title, pages, read) {
+    this.author = author,
+    this.title = title,
+    this.pages = pages,
+    this.read = read;
+}
+
+function addBook() {
+
+}
