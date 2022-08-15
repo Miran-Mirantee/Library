c87232f26cd783e462d2e265c1947de3f20c1f02
Miran-Mirantee <killerm11805@hotmail.com>
2022-08-16 01:02:02 +0700

    Added more example books and display books on webpage

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ let library = [
         title: 'Javascript is FUN',
         pages: 50,
         read: true
-    }
+    },
+    {
+        author: 'AYAYA',
+        title: 'THE WEEB WAY',
+        pages: 69,
+        read: false
+    },
+    {
+        author: 'Billy',
+        title: 'Gachi rule the WORLD',
+        pages: 6969,
+        read: true
+    },
 ];
 
 function book(author, title, pages, read) {
@@ -16,4 +28,28 @@ function book(author, title, pages, read) {
 
 function addBook() {
 
+}
+
+const shelf = document.querySelector('.bookshelf');
+
+for (const item of library) {
+    const bookInfo = document.createElement('div');
+    bookInfo.className = 'book';
+
+    const author = document.createElement('div');
+    author.className = 'author';
+    const title = document.createElement('div');
+    title.className = 'title';
+    const pages = document.createElement('div');
+    pages.className = 'pages';
+    const read = document.createElement('div');
+    read.className = 'read';
+    
+    bookInfo.append(author, title, pages, read);
+    shelf.appendChild(bookInfo);
+
+    author.textContent = item.author;
+    title.textContent = item.title;
+    pages.textContent = item.pages;
+    read.textContent = item.read;
 }
